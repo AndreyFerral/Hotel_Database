76163cc7077513659f54e5981ad01582635584fc
Andrey Yakovlev <tigra9922@gmail.com>
2021-06-05 19:50:54 +1000

    Добавьте файлы проекта.

---
+++ Hotel_Database/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Hotel_Database
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idNumberPlacement = 1;
+
+            Hide();
+            Form3 form3 = new Form3(idNumberPlacement);
+            form3.ShowDialog();
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int idNumberComfort = 2;
+
+            Hide();
+            Form3 form3 = new Form3(idNumberComfort);
+            form3.ShowDialog();
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int idNumberService = 3;
+
+            Hide();
+            Form3 form3 = new Form3(idNumberService);
+            form3.ShowDialog();
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int idNumberStaff = 4;
+
+            Hide();
+            Form3 form3 = new Form3(idNumberStaff);
+            form3.ShowDialog();
+            Close();
+        }
+    }
+}
