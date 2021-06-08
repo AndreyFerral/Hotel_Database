04af432182c91ab3097bb964846e32b2404a58aa
Andrey Yakovlev <tigra9922@gmail.com>
2021-06-08 16:01:27 +1000

    доделал бронирование процедуры, остались только двойные

--- Hotel_Database/Form14.cs
+++ Hotel_Database/Form14.cs
@@ -57,17 +57,213 @@ namespace Hotel_Database
 
         private void add_ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            try
+            {
+                // Берем значения для добавления из предпоследней строчки (последняя - пустая)
+                indexSelectRow = dataGridView1.Rows.Count - 2;
+                idReservation = dataGridView1[0, indexSelectRow].Value.ToString();
+                roomNumber = dataGridView1[1, indexSelectRow].Value.ToString();
+                workerName = dataGridView1[2, indexSelectRow].Value.ToString();
+                clientName = dataGridView1[3, indexSelectRow].Value.ToString();
+                dateReservation = dataGridView1[4, indexSelectRow].Value.ToString();
+                scheduledDateArrival = dataGridView1[5, indexSelectRow].Value.ToString();
+                scheduledDateLeave = dataGridView1[6, indexSelectRow].Value.ToString();
+                peopleNum = dataGridView1[7, indexSelectRow].Value.ToString();
+                reservationType = dataGridView1[8, indexSelectRow].Value.ToString();
+                reservaionPrice = dataGridView1[9, indexSelectRow].Value.ToString();
 
+                // надо фиксить - если запятой не будет, то исключение
+                // удаляем лишние знаки
+                /*
+                reservaionPrice = pricePerStay.Substring(0, pricePerStay.LastIndexOf(','));
+                */
+
+                myConn.Open();
+                // Остальные поля могут быть нулевыми
+                if (roomNumber.Trim() == "" || workerName.Trim() == "" ||
+                    clientName.Trim() == "" || dateReservation.Trim() == "" ||
+                    scheduledDateArrival.Trim() == "" || scheduledDateLeave.Trim() == "") throw new Exception();
+
+                // Если три последних поля оставить пустыми, то они установятся по умолчанию
+                string procedure = "execute add_reservation @workerName = @p1, @clientName = @p2, @room = @p3, " +
+                    "@dateReservation = @p4, @scheduledDateArrival = @p5, @scheduledDateLeave = @p6";
+                if (peopleNum != "") procedure = procedure + ", @peopleNum = @p7";
+                if (reservationType != "") procedure = procedure + ", @reservationType = @p8";
+                if (reservaionPrice != "") procedure = procedure + ", @reservaionPrice = @p9";
+
+                // Создать команду для добавления
+                SqlCommand myComm = new SqlCommand(procedure, myConn);
+
+                // Создать параметр и передать в него значение текстового поля 
+                myComm.Parameters.Add("@p1", SqlDbType.NVarChar, 100);
+                myComm.Parameters["@p1"].Value = workerName;
+
+                myComm.Parameters.Add("@p2", SqlDbType.NVarChar, 100);
+                myComm.Parameters["@p2"].Value = clientName;
+
+                myComm.Parameters.Add("@p3", SqlDbType.NVarChar, 100);
+                myComm.Parameters["@p3"].Value = roomNumber;
+
+                myComm.Parameters.Add("@p4", SqlDbType.SmallDateTime, 100);
+                myComm.Parameters["@p4"].Value = dateReservation;
+                
+                myComm.Parameters.Add("@p5", SqlDbType.SmallDateTime, 100);
+                myComm.Parameters["@p5"].Value = scheduledDateArrival;
+                
+                myComm.Parameters.Add("@p6", SqlDbType.SmallDateTime, 100);
+                myComm.Parameters["@p6"].Value = scheduledDateLeave;
+                
+                if (peopleNum != "") {
+                    myComm.Parameters.Add("@p7", SqlDbType.NVarChar, 100);
+                    myComm.Parameters["@p7"].Value = peopleNum;
+                }
+
+                if (reservationType != "") {
+                    myComm.Parameters.Add("@p8", SqlDbType.NVarChar, 100);
+                    myComm.Parameters["@p8"].Value = reservationType;
+                }
+
+                if (reservaionPrice != "") {
+                    myComm.Parameters.Add("@p9", SqlDbType.NVarChar, 100);
+                    myComm.Parameters["@p9"].Value = reservaionPrice;
+                }
+
+                // Выполнить запрос на изменение без возвращения результата
+                myComm.ExecuteNonQuery();
+                myConn.Close();
+
+                // Обновляем содержимое 
+                loadData();
+            }
+            catch
+            {
+                myConn.Close();
+                MessageBox.Show("Ошибка. Возможное решение:\n\n " +
+                                " 1. Возможно вы пытаетесь добавить пустую строку.", "Внимание!");
+            }
         }
 
         private void delete_ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            try
+            {
+                DialogResult result = MessageBox.Show("Данная информация будет удалена. Продолжить?", "Внимание!", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    myConn.Open();
+                    if (idReservation == "") throw new Exception();
 
+                    // Создать команду для удаления
+                    SqlCommand myComm = new SqlCommand("execute delete_reservation @p1", myConn);
+
+                    // Создать параметр и передать в него значение текстового поля 
+                    myComm.Parameters.Add("@p1", SqlDbType.NVarChar, 100);
+                    myComm.Parameters["@p1"].Value = idReservation;
+
+                    // Выполнить запрос на удаление без возвращения результата
+                    myComm.ExecuteReader();
+                    myConn.Close();
+
+                    // Обновляем содержимое 
+                    loadData();
+                }
+            }
+            catch
+            {
+                myConn.Close();
+                MessageBox.Show("Ошибка. Возможное решение:\n\n " +
+                                " 1. Возможно вы пытаетесь удалить пустую строку.", "Внимание!");
+            }
         }
 
         private void update_ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            try
+            {
+                // Берем значения для добавления из предпоследней строчки (последняя - пустая)
+                idReservation = dataGridView1[0, indexSelectRow].Value.ToString();
+                roomNumber = dataGridView1[1, indexSelectRow].Value.ToString();
+                workerName = dataGridView1[2, indexSelectRow].Value.ToString();
+                clientName = dataGridView1[3, indexSelectRow].Value.ToString();
+                dateReservation = dataGridView1[4, indexSelectRow].Value.ToString();
+                scheduledDateArrival = dataGridView1[5, indexSelectRow].Value.ToString();
+                scheduledDateLeave = dataGridView1[6, indexSelectRow].Value.ToString();
+                peopleNum = dataGridView1[7, indexSelectRow].Value.ToString();
+                reservationType = dataGridView1[8, indexSelectRow].Value.ToString();
+                reservaionPrice = dataGridView1[9, indexSelectRow].Value.ToString();
 
+                // надо фиксить - если запятой не будет, то исключение
+                // удаляем лишние знаки
+                /*
+                reservaionPrice = pricePerStay.Substring(0, pricePerStay.LastIndexOf(','));
+                */
+
+                myConn.Open();
+                // Остальные поля могут быть нулевыми
+                if (roomNumber.Trim() == "" || workerName.Trim() == "" ||
+                    clientName.Trim() == "" || dateReservation.Trim() == "" ||
+                    scheduledDateArrival.Trim() == "" || scheduledDateLeave.Trim() == "") throw new Exception();
+
+                // Если три последних поля оставить пустыми, то они установятся по умолчанию
+                string procedure = "execute update_reservation @idReservation = @p10, @workerName = @p1, @clientName = @p2, @room = @p3, " +
+                    "@dateReservation = @p4, @scheduledDateArrival = @p5, @scheduledDateLeave = @p6";
+                if (peopleNum != "") procedure = procedure + ", @peopleNum = @p7";
+                if (reservationType != "") procedure = procedure + ", @reservationType = @p8";
+                if (reservaionPrice != "") procedure = procedure + ", @reservaionPrice = @p9";
+
+                // Создать команду для добавления
+                SqlCommand myComm = new SqlCommand(procedure, myConn);
+
+                // Создать параметр и передать в него значение текстового поля 
+                myComm.Parameters.Add("@p1", SqlDbType.NVarChar, 100);
+                myComm.Parameters["@p1"].Value = workerName;
+
+                myComm.Parameters.Add("@p2", SqlDbType.NVarChar, 100);
+                myComm.Parameters["@p2"].Value = clientName;
+
+                myComm.Parameters.Add("@p3", SqlDbType.NVarChar, 100);
+                myComm.Parameters["@p3"].Value = roomNumber;
+
+                myComm.Parameters.Add("@p4", SqlDbType.SmallDateTime, 100);
+                myComm.Parameters["@p4"].Value = dateReservation;
+
+                myComm.Parameters.Add("@p5", SqlDbType.SmallDateTime, 100);
+                myComm.Parameters["@p5"].Value = scheduledDateArrival;
+
+                myComm.Parameters.Add("@p6", SqlDbType.SmallDateTime, 100);
+                myComm.Parameters["@p6"].Value = scheduledDateLeave;
+
+                if (peopleNum != "") {
+                    myComm.Parameters.Add("@p7", SqlDbType.NVarChar, 100);
+                    myComm.Parameters["@p7"].Value = peopleNum;
+                }
+
+                if (reservationType != "") {
+                    myComm.Parameters.Add("@p8", SqlDbType.NVarChar, 100);
+                    myComm.Parameters["@p8"].Value = reservationType;
+                }
+
+                if (reservaionPrice != "") {
+                    myComm.Parameters.Add("@p9", SqlDbType.NVarChar, 100);
+                    myComm.Parameters["@p9"].Value = reservaionPrice;
+                }
+
+                myComm.Parameters.Add("@p10", SqlDbType.NVarChar, 100);
+                myComm.Parameters["@p10"].Value = idReservation;
+
+                // Выполнить запрос на изменение без возвращения результата
+                myComm.ExecuteNonQuery();
+                myConn.Close();
+
+                // Обновляем содержимое 
+                loadData();
+            }
+            catch
+            {
+                myConn.Close();
+                MessageBox.Show("Ошибка. Возможное решение:\n\n " +
+                                " 1. Возможно вы пытаетесь редактировать пустую строку.", "Внимание!");
+            }
         }
 
         private void info_ToolStripMenuItem_Click(object sender, EventArgs e)
@@ -157,6 +353,7 @@ namespace Hotel_Database
             dtbl.Columns[6].AllowDBNull = true;
             dtbl.Columns[7].AllowDBNull = true;
             dtbl.Columns[8].AllowDBNull = true;
+            dtbl.Columns[9].AllowDBNull = true;
 
             // Устанавливаем значения по умолчанию переменным (первая строка)
             indexSelectRow = 0;
